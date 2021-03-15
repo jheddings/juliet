1a5dd227ef7f7b6d324fd928014fcf7b434772d4
jheddings <jason@heddings.com>
2021-03-15 00:22:09 -0600

    Bi-directional ping support

--- src/juliet/server.py
+++ src/juliet/server.py
@@ -1,12 +1,14 @@
 import socket
 import threading
 import re
+import time
 import logging
 import juliet
 
 from datetime import datetime, timezone
 
 SOCKET_BUFFER_SIZE = 1024
+SESSION_TIMEOUT_SEC = 300
 
 regex_eol = re.compile(rb'\r?\n')
 regex_nickname = re.compile(rb'[a-zA-Z0-9|`\\_{}\[\]-]+')
@@ -26,15 +28,14 @@ class Session(object):
         self.nickname = None
         self.fullname = None
 
-        host, port = socket.getpeername()
-        self.hostname = host
-        self.hostport = port
-
-        self.registration_pending = None
-
+        self.pending_welcome = None
+        self.authorized = (server.password is None)
+        self.host, self.port = socket.getpeername()
         self.logger = logging.getLogger('juliet.Session')
+        self.send_lock = threading.Lock()
 
         # create event handlers
+        self.on_away = juliet.Event()
         self.on_motd = juliet.Event()
         self.on_nick = juliet.Event()
         self.on_ping = juliet.Event()
@@ -72,61 +73,70 @@ class Session(object):
         self.fullname = None if len(parts) < 2 else parts[1].decode('utf-8')
 
         if self.nickname is None:
-            self.registration_pending = True
+            self.pending_welcome = True
         else:
-            self.process_registration()
+            self.send_welcome()
 
         self.on_user(self, self.username)
 
     #---------------------------------------------------------------------------
     def handle_nick(self, nick):
         if nick is None:
-            self.notice('431 :Missing nickname')
+            self.notify('431 :Missing nickname')
 
         elif self.server.get_session(nick):
-            self.notice('433 * %s :Nickname in use', nick)
+            self.notify('433 * %s :Nickname in use', nick)
 
         elif not regex_nickname.match(nick):
-            self.notice('432 * %s :Invalid nickname', nick)
+            self.notify('432 * %s :Invalid nickname', nick)
 
         else:
             self.nickname = nick.decode('utf-8')
             self.logger.info('registered nickname -- %s', self.nickname)
 
-            if self.registration_pending:
-                self.process_registration()
+            if self.pending_welcome:
+                self.send_welcome()
 
             self.on_nick(self, self.nickname)
 
     #---------------------------------------------------------------------------
     def handle_away(self, away):
-        self.away = away
-
         if away:
-            away = away.decode('utf-8')
+            self.away = away.decode('utf-8')
             self.logger.info('@%s is away -- %s', self.nickname, away)
         else:
+            self.away = False
             self.logger.info('@%s is not away', self.nickname)
+
+        self.on_away(self, away)
 
     #---------------------------------------------------------------------------
     def handle_motd(self):
         if self.server.motd is None:
-            self.notice('422 {} :MOTD is missing', self.nickname)
+            self.notify('422 {} :MOTD is missing', self.nickname)
         else:
-            self.notice('375 {} :- {} Message of the day -', self.nickname, self.server.name)
-            self.notice('372 {} :- {}', self.nickname, self.server.motd.strip())
-            self.notice('376 {} :End of MOTD command', self.nickname)
+            self.notify('375 {} :- {} Message of the day -', self.nickname, self.server.name)
+            self.notify('372 {} :- {}', self.nickname, self.server.motd.strip())
+            self.notify('376 {} :End of MOTD command', self.nickname)
 
         self.on_motd(self, self.server.motd)
 
     #---------------------------------------------------------------------------
     def handle_ping(self, args):
         if args is None:
-            self.notice('409 {} :Missing origin', self.nickname)
+            self.notify('409 {} :Missing origin', self.nickname)
         else:
             token = args.decode('utf-8')
-            self.notice('PONG {} :{}', self.server.name, token)
+            self.notify('PONG {} :{}', self.server.name, token)
             self.on_ping(self, token)
+
+    #---------------------------------------------------------------------------
+    def handle_pong(self, args):
+        if args is None:
+            self.notify('409 {} :Missing origin', self.nickname)
+        else:
+            token = args.decode('utf-8')
+            self.on_pong(self, token)
 
     #---------------------------------------------------------------------------
     def handle_quit(self, reason):
@@ -142,6 +152,8 @@ class Session(object):
     #---------------------------------------------------------------------------
     def handle_command(self, command, args):
         if command is None: return
+
+        # TODO if server has password, check session authorized before other commands
 
         command = command.upper()
         self.logger.debug('command -- %s [%s]', command, args)
@@ -187,47 +199,59 @@ class Session(object):
                 self.logger.error('error processing command', exc_info=True)
 
     #---------------------------------------------------------------------------
-    def notice(self, msg, *args):
+    def send(self, msg, *args):
         if args is not None:
             msg = msg.format(*args)
 
-        string = f':{self.server.name} {msg}\r\n'
+        resp = f'{msg}\r\n'
 
-        self.logger.debug('>> %s', msg)
-        self.socket.send(bytes(string, 'utf-8'))
+        with self.send_lock:
+            self.logger.debug('>> %s', msg)
+            self.socket.send(bytes(resp, 'utf-8'))
 
     #---------------------------------------------------------------------------
-    def process_registration(self):
+    def send_ping(self):
+        self.send('PING {}', self.server.hostname)
+
+    #---------------------------------------------------------------------------
+    def notify(self, msg, *args):
+        if args is not None:
+            msg = msg.format(*args)
+
+        self.send(':{} {}', self.server.name, msg)
+
+    #---------------------------------------------------------------------------
+    def send_welcome(self):
         self.logger.info('Registering new user -- %s @%s', self.username, self.nickname)
 
-        self.notice(
+        self.notify(
             '001 {} :Welcome to the mesh IRC network {} -- {}@{}',
-            self.nickname, self.nickname, self.username, self.hostname
+            self.nickname, self.nickname, self.username, self.host
         )
 
-        self.notice(
+        self.notify(
             '002 {} :Your host is {}, running juliet-{}',
             self.nickname, self.server.name, juliet.VERSION
         )
 
-        self.notice(
+        self.notify(
             '003 {} :This server was created {}',
             self.nickname, self.server.start_time
         )
 
-        self.notice(
+        self.notify(
             '004 {} {} juliet-{} o o',
             self.nickname, self.server.name, juliet.VERSION
         )
 
-        #self.send_lusers()
         self.handle_motd()
 
-        self.registration_pending = False
+        self.pending_welcome = False
 
     #---------------------------------------------------------------------------
     def _thread_worker(self):
-        self.logger.debug('starting new session')
+        self.logger.debug('session [%s] started from %s:%d', id(self), self.host, self.port)
+
         self.active = True
 
         while self.active and self.socket:
@@ -257,14 +281,17 @@ class Session(object):
 class Server(object):
 
     #---------------------------------------------------------------------------
-    def __init__(self, address='', port=6667, motd=None, server_name=None):
+    def __init__(self, address='', port=6667, motd=None, password=None, server_name=None):
         self.address = address
         self.port = port
         self.motd = motd
+        self.password = password
 
         self.socket = None
         self.sessions = list()
+        self.session_lock = threading.Lock()
 
+        self.active = None
         self.start_time = datetime.now(tz=timezone.utc)
 
         server_name_limit = 63  # From the RFC.
@@ -282,23 +309,34 @@ class Server(object):
         self.logger.debug('starting server -- %s:%d', self.address, self.port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        ping_thread = threading.Thread(
+            name=f'SessionPing', target=self._ping_worker, daemon=True
+        )
+
+        self.active = True
+
         with self.socket:
             self.socket.bind((self.address, self.port))
             self.socket.listen()
             self.logger.info('Server started -- %s:%d', self.address, self.port)
 
-            while True:
+            ping_thread.start()
+
+            while self.active:
                 conn, addr = self.socket.accept()
                 self.logger.info('Client connected -- %s:%d', addr[0], addr[1])
 
                 session = Session(self, conn)
-                self.sessions.append(session)
+                with self.session_lock:
+                    self.sessions.append(session)
 
+        ping_thread.join()
         self.logger.info('Server stopped')
 
     #---------------------------------------------------------------------------
     def stop(self):
         self.logger.debug('stopping server')
+        self.active = False
 
         # TODO close sessions gracefully
 
@@ -309,24 +347,51 @@ class Server(object):
         self.socket.close()
 
     #---------------------------------------------------------------------------
-    def send(self, msg):
-        self.logger.debug('stopping server')
-        self.socket.sendall(msg)
-
-    #---------------------------------------------------------------------------
     def remove_session(self, nick):
+        self.logger.debug('removing session -- %s', nick)
+
         session = self.get_session(nick)
 
         if session is None:
             self.logger.warning('Invalid session; nickname not found: %s', nick)
         else:
-            self.sessions.remove(session)
+            with self.session_lock:
+                self.sessions.remove(session)
 
     #---------------------------------------------------------------------------
     def get_session(self, nick):
-        for session in self.sessions:
-            if session.nickname == nick:
-                return session
+        self.logger.debug('looking for session -- %s', nick)
+
+        with self.session_lock:
+            for session in self.sessions:
+                if session.nickname == nick:
+                    return session
 
         return None
+
+    #---------------------------------------------------------------------------
+    def _ping_worker(self):
+        self.logger.debug('ping thread starting')
+
+        while self.active:
+            with self.session_lock:
+                now = datetime.now(tz=timezone.utc)
+                self.logger.debug('checking active sessions -- %s', now)
+
+                for session in self.sessions:
+                    last_contact = now - session.last_contact
+                    total_sec = last_contact.total_seconds()
+
+                    self.logger.debug('session [%s] last contact: %s sec', id(session), last_contact)
+
+                    if total_sec > SESSION_TIMEOUT_SEC * 1.25:
+                        self.logger.debug('client timeout')
+                        session.close()
+                    elif total_sec > SESSION_TIMEOUT_SEC:
+                        self.logger.debug('client inactive')
+                        session.send_ping()
+
+            time.sleep(SESSION_TIMEOUT_SEC / 2)
+
+        self.logger.debug('ping thread finished')
 
